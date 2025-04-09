4367682bbbbfc17e21c736d58d10c38b1a19b2ac
steiltre <trevor.steil1@gmail.com>
2025-04-08 20:07:48 -0700

    Adding render-markdown and markdown preview to Neovim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -46,7 +46,23 @@ return {
     build = "cd app && yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
-    end,
+     end,
     ft = { "markdown" },
+  }, 
+  -- install without yarn or npm
+  --{
+  --    "iamcco/markdown-preview.nvim",
+  --    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --    ft = { "markdown" },
+  --    build = function() vim.fn["mkdp#util#install"]() end,
+  --},
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
   }
 }
