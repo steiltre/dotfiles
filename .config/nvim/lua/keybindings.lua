adb09f900da284bdf63948b78cec8de2f7a79e66
Trevor Steil <trevor.steil1@gmail.com>
2026-04-13 16:43:45 -0700

    Updating LSP config for NeoVim

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -16,12 +16,12 @@ vim.keymap.set('n', '<leader>mp', '<Plug>MarkdownPreviewToggle', {})
 local ls = require("luasnip")
 
 -- LuaSnip
---vim.keymap.set({"i"}, "<C-H>", function() ls.expand() end, {silent = true})
---vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump( 1) end, {silent = true})
---vim.keymap.set({"i", "s"}, "<C-K>", function() ls.jump(-1) end, {silent = true})
---
---vim.keymap.set({"i", "s"}, "<C-E>", function()
---	if ls.choice_active() then
---		ls.change_choice(1)
---	end
---end, {silent = true})
+vim.keymap.set({"i"}, "<C-H>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-K>", function() ls.jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-E>", function()
+if ls.choice_active() then
+	ls.change_choice(1)
+end
+end, {silent = true})
